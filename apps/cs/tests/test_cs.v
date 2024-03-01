f4396ff38ca5bd68579e047739504e28b7399f65
Quentin Vermande <quentin.vermande@orange.fr>
2024-03-01 14:46:17 +0100

    insert last version of cs hook

---
+++ apps/cs/tests/test_cs.v
@@ -0,0 +1,32 @@
+From elpi.apps Require Import cs.
+From Coq Require Import Bool.
+
+Elpi Override CS All.
+
+Structure S (T : Type) : Type :=
+  { sort :> T -> T }.
+
+Elpi Accumulate canonical_solution lp:{{
+
+cs _ L R S :-
+  coq.say "enter cs " L R, fail.
+cs _ {{ sort lp:T }} {{ @id lp:T }} {{ Build_S lp:T (@id lp:T) }}.
+
+}}.
+Elpi Typecheck canonical_solution.
+
+Check 1.
+Set Debug "elpi-unification".
+Check eq_refl _ : (sort nat _) = @id nat.
+Check 11.
+Check eq_refl _ : (sort nat _) 1 = @id nat 1.
+Definition id1 := id.
+Check 2.
+Check eq_refl _ : (sort nat _) = @id1 nat.
+Definition sort1 := sort.
+Check 3.
+Check eq_refl _ : (sort1 nat _) = @id nat.
+Check 4.
+Check eq_refl _ : (sort1 nat _) = @id1 nat.
+
+
